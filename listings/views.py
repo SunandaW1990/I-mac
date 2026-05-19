a10b9146893db15ec22eb950f05db381674b9ee3
Sunanda.W <Sunanda.W1990@gmail.com>
2026-05-19 12:52:16 +0800

    0519 before lunch

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import Paginator
+from django.db.models import Q
+from .choices import district_choices, room_type_choices, rooms_choices
 # Create your views here.
 
 # Windsurf: Refactor | Explain | Generate Docstring | X
 def listings(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.filter(is_published=True).order_by
     paginator = Paginator(listings, 3)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
@@ -18,3 +20,26 @@ def listing(request, listing_id):
     context = {"listing": listing}
     return render(request, "listings/listing.html", context )
 
+def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(Q(description__icontains=keywords) | Q(title_icontains=keywords) | Q(doctor_name_icontains=keywords))
+    if 'district' in request.GET:
+        district = request.GET['district']
+        if district:
+            queryset_list = queryset_list.filter(district__iexact=district)
+    if 'rooms' in request.GET:
+        rooms = request.GET['rooms']
+        if rooms:
+            queryset_list = queryset_list.filter(rooms__gte=rooms)
+    if 'room_type' in request.GET:
+        room_type = request.GET['room_type']
+        if room_type:
+            queryset_list = queryset_list.filter(room_type__iexact=room_type)
+    paginator = Paginator(queryset_list, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = {"listings": paged_listings, "district_choices":district_choices, "room_type_choices":room_type_choices, "rooms_choices":rooms_choices, "values":request.GET }
+    return render(request, "listings/search.html", context)
